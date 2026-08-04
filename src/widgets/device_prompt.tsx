794a9a29dc339a4c88bf77ae7c7057f5d19d7ffa
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-08-04 21:42:12 +0800

    Initialize Commit

---
+++ src/widgets/device_prompt.tsx
@@ -0,0 +1,8 @@
+import USBPrompt from '../assets/usb-data-transfer-svgrepo-com.svg';
+
+export function InsertDevice(){
+    <div>
+        <img src={USBPrompt}/>
+        <h1>Inser USB</h1>
+    </div>
+}
