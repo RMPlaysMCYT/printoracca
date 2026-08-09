eab2cf4c053545384ce43edca6ca997f05a8e73f
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-08-09 21:14:28 +0800

    add new changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,37 @@ import { useTauriInfo } from "./widgets/tauri_version";
 import { InsertDevice } from "./components/ui/device_prompt";
 
 function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const [name, setName] = useState("");
-    const [showDevicePrompt, setShowDevicePrompt] = useState(false);
-    const { isTauriApp, appVersion, isLoading } = useTauriInfo();
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+  const [showDevicePrompt, setShowDevicePrompt] = useState(false);
+  const { isTauriApp, appVersion, isLoading } = useTauriInfo();
 
-    async function greet() {
-        setGreetMsg(await invoke("greet", { name }));
-    }
+  async function greet() {
+    setGreetMsg(await invoke("greet", { name }));
+  }
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <main className="container">
-            <div className="content">
-                <h1>PROJECT PISO PRINT</h1>
-                
-                {showDevicePrompt ? (
-                    <InsertDevice onClose={() => setShowDevicePrompt(false)} />
-                ) : (
-                    <button onClick={() => setShowDevicePrompt(true)}>Start</button>
-                )}
-                
-                {greetMsg && <p>{greetMsg}</p>}
-            </div>
-            <AppFooter appVersion={null} isTauriApp={true} />
-        </main>
-    );
+  return (
+    <main className="container">
+      <div className="content">
+        <div className="TitleCard">
+          <h1>PROJECT PISO PRINT</h1>
+        </div>
+
+        {showDevicePrompt ? (
+          <InsertDevice onClose={() => setShowDevicePrompt(false)} />
+        ) : (
+          <button onClick={() => setShowDevicePrompt(true)}>Start</button>
+        )}
+
+        {greetMsg && <p>{greetMsg}</p>}
+      </div>
+      <AppFooter appVersion={null} isTauriApp={true} />
+    </main>
+  );
 }
 
 export default App;
