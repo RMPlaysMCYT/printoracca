99226f335774d44add88aad09f4639c827ae6faf
RMPlaysMCYT <ronnel.mitra.77@gmail.com>
2026-08-25 10:20:12 +0800

    import css parts

--- src/components/file_explorer.tsx
+++ src/components/file_explorer.tsx
@@ -5,7 +5,8 @@ import { invoke } from '@tauri-apps/api/core';
 import { renderAsync } from "docx-preview";
 import { Document, Page, pdfjs } from "react-pdf";
 
-
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
