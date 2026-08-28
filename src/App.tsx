72fec5208be6a4c367e6a9d8aa3e25a56a984762
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-08-28 15:39:37 +0800

    fixing the design

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function App() {
 
   return (
     <main className="container">
-      <div className="content">
+      <div className="content"> 
         <div className="TitleCard">
           <h1>PROJECT PISO PRINT 1.0.0</h1>
         </div>
